3a95ad50adf6ac0f5788e4791dd11ee0724d4546
SAnchous8 <sano2006.yande@yandex.ru>
2026-04-26 16:40:33 +0500

    Class1.cs

--- Task1/Csharp/Class1.cs
+++ Task1/Csharp/Class1.cs
@@ -1,63 +1,87 @@
 public class ThreeNumbers
 {
-    private int number1;
-    private int number2;
-    private int number3;
+    private int _number1;
+    private int _number2;
+    private int _number3;
 
     public ThreeNumbers()
     {
-        number1 = 0;
-        number2 = 0;
-        number3 = 0;
+        _number1 = 0;
+        _number2 = 0;
+        _number3 = 0;
     }
 
     public ThreeNumbers(int a, int b, int c)
     {
-        number1 = a;
-        number2 = b;
-        number3 = c;
+        _number1 = a;
+        _number2 = b;
+        _number3 = c;
     }
 
     public ThreeNumbers(ThreeNumbers other)
     {
-        number1 = other.number1;
-        number2 = other.number2;
-        number3 = other.number3;
+        _number1 = other._number1;
+        _number2 = other._number2;
+        _number3 = other._number3;
     }
 
     public int Number1
     {
-        get { return number1; }
-        set { number1 = value; }
+        get
+        {
+            return _number1;
+        }
+        set
+        {
+            _number1 = value;
+        }
     }
 
     public int Number2
     {
-        get { return number2; }
-        set { number2 = value; }
+        get
+        {
+            return _number2;
+        }
+        set
+        {
+            _number2 = value;
+        }
     }
 
     public int Number3
     {
-        get { return number3; }
-        set { number3 = value; }
+        get 
+        {
+            return _number3;
+        }
+        set
+        {
+            _number3 = value;
+        }
     }
 
     public int GetMaxLastDigit()
     {
-        int digit1 = Math.Abs(number1 % 10);
-        int digit2 = Math.Abs(number2 % 10);
-        int digit3 = Math.Abs(number3 % 10);
+        int digit1 = Math.Abs(_number1 % 10);
+        int digit2 = Math.Abs(_number2 % 10);
+        int digit3 = Math.Abs(_number3 % 10);
 
         int max = digit1;
-        if (digit2 > max) max = digit2;
-        if (digit3 > max) max = digit3;
+        if (digit2 > max)
+        {
+            max = digit2;
+        }
+        if (digit3 > max)
+        {
+            max = digit3;
+        }
 
         return max;
     }
 
     public override string ToString()
     {
-        return "Numbers: " + number1 + ", " + number2 + ", " + number3;
+        return "Numbers: " + _number1 + ", " + _number2 + ", " + _number3;
     }
 }
